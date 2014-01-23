e444e81c46c085a191e8eec5b73ba45763b7dfbb
Stefan Keim <42o8o1oo@googlemail.com>
2014-01-23 18:17:03 +0100

    SVG Falcon

---
+++ UI/app/scripts/controllers/playground.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+angular.module('uavRcApp')
+  .controller('PlaygroundCtrl', function ($scope, socket) {
+      socket.forward('GPS_DATA', $scope);
+      socket.forward('connect', $scope);
+
+      $scope.goto = {
+          latitude: 0,
+          longitude: 0.0001,
+          height: 50
+      }
+
+      $scope.send = function () {
+          console.log('COMMAND_GOTO', $scope.goto);
+          socket.emit('COMMAND_GOTO', $scope.goto);
+      }
+      $scope.$on('socket:connect', function (data) {
+          console.log("test");
+          //console.log(data);
+      })
+
+      $scope.$on('socket:GPS_DATA', function (ev, data) {
+          $scope.GPS_DATA = data;
+      });
+
+
+  });
