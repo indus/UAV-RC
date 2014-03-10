8bbb92d9f0e0eddfccf9010d9916e803bdc3f152
keim_se <Stefan.Keim@googlemail.com>
2014-03-10 15:10:56 +0100

    debug with predefined signals

--- UI/app/scripts/controllers/debug.js
+++ UI/app/scripts/controllers/debug.js
@@ -6,9 +6,72 @@ angular.module('uavRcApp')
       $scope.msgs = [];
       $scope.log = [];
 
+      $scope.predefinedSignals = [
+          {
+              signal: "UAV_STATUS_GET", body: {}, ack: true
+          },
+          {
+              signal: "UAV_GPSDATA_GET", body: {}, ack: true
+          },
+          {
+              signal: "UAV_STATUS_GET", body: {}, ack: true
+          },
+          {
+              signal: "UAV_CAM_ANGLE_GET", body: {}, ack: true
+          },
+          { signal: "UAV_ASCTEC_LL_STATUS_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_IMU_RAWDATA_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_IMU_CALCDATA_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_RC_DATA_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_CTRL_PUT_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_GPS_DATA_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_CURRENT_WAY_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_GPS_DATA_ADVANCED_GET", body: {}, ack: true },
+            { signal: "UAV_ASCTEC_CAM_DATA_GET", body: {}, ack: true },
+            { signal: "UAV_CAM_TRIGGER", body: {}, ack: true },
+            {
+                signal: "UAV_CAM_ANGLE_SET", body: {
+                    pitch: 0,
+                    roll: 0
+                }, ack: true
+            },
+            {
+                signal: "UAV_GOTO", body: {
+                    lat: 56.897123,
+                    lon: 11.123812,
+                    alt: 52, //optional
+                    heading: 13 //optional
+                }, ack: true
+            },
+            {
+                signal: "UAV_GOTOANDCAM", body: {
+                    lat: 56.897123,
+                    lon: 11.123812,
+                    alt: 52, //optional
+                    heading: 13, //optional
+                    cam: {
+                        pitch: -10, // camera-pitch in deg
+                        roll: 0, // camera-roll in deg
+                        trigger: true // trigger the camera
+                    }
+                }, ack: true
+            },
+            { signal: "UAV_START", body: {}, ack: true },
+            { signal: "UAV_STOP", body: {}, ack: true },
+            { signal: "UAV_HOME", body: {}, ack: true },
+      ];
+
       socket.on('*', function (msg) {
           $scope.log.push(msg)
       })
+
+
+
+      $scope.setPredefined = function (pre) {
+          $scope.signal = pre.signal;
+          $scope.dataString = JSON.stringify($scope.msg.body = pre.body, null, "\t")
+          $scope.msg.header.msg.ack = pre.ack;
+      }
 
       $scope.emit = function () {
           var msg = angular.copy($scope.msg);
@@ -20,7 +83,7 @@ angular.module('uavRcApp')
                   console.log(msg);
                   var date = new Date();
                   emit.ack = msg;
-                  emit.timeStringAck = ((+new Date()) - emit.timestamp)+" ms";
+                  emit.timeStringAck = ((+new Date()) - emit.timestamp) + " ms";
               }
           }
           socket.emit($scope.signal, msg, ack);
@@ -37,7 +100,7 @@ angular.module('uavRcApp')
 
           console.log(msg);
           $scope.msgs.push(emit);
-          $scope.msg.header.msg.id= Math.random().toString(36).substring(2, 11)
+          $scope.msg.header.msg.id = Math.random().toString(36).substring(2, 11)
       }
 
 
@@ -55,7 +118,7 @@ angular.module('uavRcApp')
           msg = _.ioMsg(403, null, msg);
           socket.emit('ACK', msg);
       })
-      
+
       socket.on('ackerror', function (msg) {
       })
 
